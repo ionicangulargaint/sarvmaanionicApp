3fc34bdf98b8e7a686c22d2c710aca80e62a657f
ionicangulargaint <bittusinghbca@gmail.com>
2018-04-05 21:11:33 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
+import { LoadingController, Loading } from 'ionic-angular';
+import { ApiService } from '../api-services/api.services';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { PriceListPage } from '../pages/price-list/price-list';
+import { ProfilePage } from '../pages/profile/profile';
+import { VendorRegistrationPage } from '../pages/vendor-registration/vendor-registration';
+
+
+import { AuthProvider } from '../providers/auth/auth';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,17 +21,34 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any;// = LoginPage;
+  loading: Loading;
+  loadingConfig: any;
+  profileData: any = {
+    name: '',
+    profession: '',
+    imageUrl: 'assets/imgs/user.png'
+  }
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
 
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(
+    public platform: Platform,
+    public statusBar: StatusBar,
+    public splashScreen: SplashScreen,
+    public apiServices: ApiService,
+    private loadingCtrl: LoadingController
+  ) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'PriceList', component: PriceListPage },
+      { title: 'List', component: ListPage },
+      { title: 'Login', component: LoginPage },
+      { title: 'SignUp', component: VendorRegistrationPage },
+      { title: 'Profile', component: ProfilePage }
     ];
 
   }
@@ -33,12 +59,89 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.refreshToken();
+
+    });
+  }
+
+  checkAuthorization() {
+    this.createLoader();
+    this.loading.present().then(() => {
+      this.apiServices.getUserProfile()
+        .subscribe(response => {
+          this.loading.dismiss();
+          //this.profileData = response;
+          this.profileData.name = response.appUsers.firstName + '' + response.appUsers.lastName;
+          this.profileData.profession = response.appUsers.businessDetails.skillSet[0].displayText;
+          this.profileData.imageUrl = response.appUsers.businessDetails.logo == null ? 'assets/imgs/user.png' : response.appUsers.businessDetails.logo ;
+          //this.profileData = response;
+          this.rootPage = HomePage;
+        }, (error) => {
+          this.loading.dismiss();
+          if (error.error == "invalid_token") {
+            this.refreshToken();
+            //this.rootPage = LoginPage;
+            localStorage.clear();
+          } else {
+            this.rootPage = LoginPage;
+            //this.rootPage = VendorRegistrationPage
+            localStorage.clear();
+          }
+        });
+    });
+  }
+
+
+  refreshToken() {
+    this.createLoader();
+    this.loading.present().then(() => {
+      this.apiServices.refreshToken()
+        .subscribe(response => {
+          this.loading.dismiss();
+          //this.rootPage = HomePage;
+          this.checkAuthorization();
+        }, (error) => {
+          this.loading.dismiss();
+          if (error.error == "invalid_grant") {
+            this.rootPage = LoginPage;
+            localStorage.clear();
+          }
+        });
+    });
+  }
+
+  createLoader(message: string = "Checking authentication..") { // Optional Parameter
+    this.loading = this.loadingCtrl.create({
+      content: message
     });
   }
 
   openPage(page) {
-    // Reset the content nav to have just this page
+
+    let getSelectedIndex: any;
+
+    this.pages.forEach((item, index) => {
+      if (item.title == page) {
+        getSelectedIndex = item;
+      }
+    })
+
+
+
+    //this.nav.setRoot(page.component);
+    if (localStorage.getItem('isLogin') == 'true')// page == 'Login')
+    {
+      //this.nav.setRoot(page.component);
+      this.nav.push(getSelectedIndex.component);
+    } else if (page == 'Logout') {
+      localStorage.clear();
+    }
+
+    // Otherwise reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    else {
+      localStorage.clear();
+      this.nav.setRoot(this.pages[3].component);
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,68 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+
+import { SelectSearchableModule } from 'ionic-select-searchable';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { TermModalPage } from '../pages/term-modal/term-modal';
+import { LoginPage } from '../pages/login/login';
+import { PriceListPage } from '../pages/price-list/price-list';
+import { ProfilePage } from '../pages/profile/profile';
+import { VendorRegistrationPage } from '../pages/vendor-registration/vendor-registration';
+import { AddUpdateQuotePage } from '../pages/add-update-quote/add-update-quote';
+import { EditProfilePage } from '../pages/edit-profile/edit-profile';
+import { UpdateUserInfoPage } from '../pages/update-user-info/update-user-info';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthProvider } from '../providers/auth/auth';
+import { ApiService } from '../api-services/api.services';
+import { HelperService } from '../api-services/helperServices';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    LoginPage,        
+    VendorRegistrationPage,
+    PriceListPage,
+    ProfilePage,
+    AddUpdateQuotePage,
+    EditProfilePage,
+    UpdateUserInfoPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpModule,
+    SelectSearchableModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,    
+    LoginPage,
+    VendorRegistrationPage,
+    PriceListPage,
+    ProfilePage,
+    AddUpdateQuotePage,
+    EditProfilePage,
+    UpdateUserInfoPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider,
+    ApiService,
+    HelperService
   ]
 })
 export class AppModule {}
